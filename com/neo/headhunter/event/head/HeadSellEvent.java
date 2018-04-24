4267e9c1f743b230f08ad6457847c8d12f930b77
BuildTools <neoev421@gmail.com>
2018-04-24 01:01:19 -0400

    Updated HeadSellEvent

--- com/neo/headhunter/event/head/HeadSellEvent.java
+++ com/neo/headhunter/event/head/HeadSellEvent.java
@@ -2,27 +2,23 @@ package com.neo.headhunter.event.head;
 
 import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
-import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
+import org.bukkit.event.player.PlayerEvent;
 
-public class HeadSellEvent extends Event implements Cancellable {
+public class HeadSellEvent extends PlayerEvent implements Cancellable {
 	private static final HandlerList HANDLERS = new HandlerList();
 	private boolean cancelled;
 	
-	private final Player hunter;
 	private boolean inventory;
 	private boolean fromSign;
 	
 	public HeadSellEvent(Player hunter, boolean inventory, boolean fromSign) {
+		super(hunter);
+		
 		this.cancelled = false;
 		
-		this.hunter = hunter;
 		this.inventory = inventory;
 		this.fromSign = fromSign;
-	}
-	
-	public Player getHunter() {
-		return hunter;
 	}
 	
 	public boolean isInventory() {
